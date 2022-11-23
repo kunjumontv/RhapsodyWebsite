47f18f3459cea5c4bee5dcfbd68c4c9efa83fe25
AliAlDarweshTTF <ali,darwesh@thethoughtfactory.ae>
2022-11-23 16:23:23 +0400

    Contact page desktop layout

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,11 +10,11 @@ const contact = () => {
   return (
     <>
       {/* banner  */}
-      <section className="section-box">
+      <section className="section-box mb-145">
         <div className="container mt-120">
           <div className="row">
 
-            <div className="col-lg-6 col-sm-12 block-we-do text-center text-lg-start">
+            <div className="col-lg-6 col-sm-12 text-center text-lg-start pt-80">
               <p className="text-body-tiny-2">Working alongside great brands</p>
               <h3 className="text-heading-1 text-center text-lg-start">
                 We’re committed to doing something <span className="color-green-900">extraordinary</span> every
@@ -28,27 +28,32 @@ const contact = () => {
           </div>
         </div>
         <div className="row text-center">
-          <div className="col-12 text-body-tiny-4">
+          <div className="col-12 text-body-tiny-4 mt-80">
             <p className="">Scroll for more</p>
             <FaArrowDown className="color-green-900" />
           </div>
         </div>
       </section>
 
-      <section className="section-box ">
-        <div className="container gray-light-background pt-100 pb-100 bdrd-40 mb-50">
+
+
+      {/* Second Section */}
+      <section className="section-big gray-dark-background bdrd-40 pb-80 mb-60">
+        <div className="container pt-100 pb-80 ">
+
           <div className="row">
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
-            <div className="col-lg-10 col-sm-12 col-12 block-we-do">
+
+            <div className="col-lg-12 col-sm-12 ">
               <h2 className="text-heading-1 color-gray-900 mb-50">Our team of experts are ready to answer your questions.</h2>
             </div>
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
+
+
           </div>
 
+          <div className="row mt-85">
 
-          <div className="row pt-50">
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
-            <div className="col-lg-5 col-md-6 col-sm-6 col-12 block-we-do">
+
+            <div className="col-lg-6 col-md-6 col-sm-6 col-12">
               <div className="">
                 <div className="">
                   <div className="pb-20">
@@ -73,7 +78,7 @@ const contact = () => {
                 </div>
               </div>
             </div>
-            <div className="col-lg-5 col-md-6 col-sm-6 col-12 block-we-do">
+            <div className="col-lg-6 col-md-6 col-sm-6 col-12">
               <form>
                 <div className="col-lg-12 ">
                   <div className="form-group"><input className="form-control" placeholder="Enter your name" /></div>
@@ -96,21 +101,19 @@ const contact = () => {
                 </div>
               </form>
             </div>
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
+
+
           </div>
 
-          <div className="row pt-50">
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
-            <div className="col-lg-10 col-sm-12 col-12 block-we-do">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.1413976843196!2d55.166019114812464!3d25.097074483942844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6b6fa1e692b7%3A0xd36d15785b1bd3df!2sThe%20Onyx%20Tower%201!5e0!3m2!1sen!2sae!4v1669104080664!5m2!1sen!2sae" width="100%" height="800"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div className="row mt-85">
+
+            <div className="col-lg-12 col-sm-12">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.1413976843196!2d55.166019114812464!3d25.097074483942844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6b6fa1e692b7%3A0xd36d15785b1bd3df!2sThe%20Onyx%20Tower%201!5e0!3m2!1sen!2sae!4v1669104080664!5m2!1sen!2sae" width="100%" height="800" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
             </div>
-            <div className="col-lg-1 col-sm-1 col-12 d-none d-lg-block" />
           </div>
         </div>
-      </section >
-
-
-
+      </section>
     </>
   )
 }
